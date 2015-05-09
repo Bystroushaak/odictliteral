0094ffa1a7f63277827accf5cd85144bdece96a9
Bystroushaak <bystrousak@kitakitsune.org>
2015-05-09 17:35:17 +0200

    Improved readability, PEP8 aplied. Added more imports of Iterable.

--- odictliteral.py
+++ odictliteral.py
@@ -1,21 +1,27 @@
 try:
     from collections import OrderedDict
-except:
+except ImportError:
     from ordereddict import OrderedDict
 
 try:
     from reprlib import recursive_repr
-except:
+except ImportError:
     # don't cope with recursive repr calls in py2
-    def recursive_repr(fillvalue='...'): return (lambda f: f)
+    def recursive_repr(fillvalue='...'):
+        return (lambda f: f)
 
 try:
-    from collections.abc import Iterable
-except:
-    Iterable = tuple
+    from collections import Iterable
+except ImportError:
+    try:
+        from collections.abc import Iterable
+    except ImportError:
+        Iterable = tuple
+
 
 __all__ = ["odict"]
-__version__ = '1.0.0'
+__version__ = '1.0.1'
+
 
 class odictType(type):
     syntax_error = SyntaxError("Allowed syntax: odict[<k>: <v>(, <k>: <v>...)]")
@@ -25,18 +31,25 @@ class odictType(type):
             keys = (keys,)
         if not isinstance(keys, Iterable):
             raise self.syntax_error
+
         od = self()
         for k in keys:
             if not isinstance(k, slice) or k.step is not None:
                 raise self.syntax_error
+
             od[k.start] = k.stop
+
         return od
+
 
 @recursive_repr(fillvalue="odict[...]")
 def odict_repr(self):
-    if len(self) == 0:
+    if not self:
         return "odict()"
-    else:
-        return "odict[%s]" % (", ".join("%r: %r" % (k,v) for k,v in self.items()),)
+
+    return "odict[%s]" % (", ".join(
+        "%r: %r" % (k, v)
+        for k, v in self.items()
+    ),)
 
 odict = odictType(str('odict'), (OrderedDict,), {"__repr__": odict_repr})
