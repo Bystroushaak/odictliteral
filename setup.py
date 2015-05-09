bfe93e1d52a84d541a3169057336107430031b49
Bystroushaak <bystrousak@kitakitsune.org>
2015-05-09 17:35:25 +0200

    setup.py: PEP8 applied, added pytest to tests_require directive.

--- setup.py
+++ setup.py
@@ -1,15 +1,19 @@
 #!/usr/bin/env python
+import re
+import codecs
+import os.path
 
-from setuptools import setup, find_packages
-
-import codecs, os.path, re
+from setuptools import setup
 
 here = os.path.abspath(os.path.dirname(__file__))
 
-# Read the version number from a source file.
-# Why read it, and not import?
-# see https://groups.google.com/d/topic/pypa-dev/0PkjVpcxTzQ/discussion
+
 def find_version(*file_paths):
+    """
+    Read the version number from a source file.
+    Why read it, and not import?
+    see https://groups.google.com/d/topic/pypa-dev/0PkjVpcxTzQ/discussion
+    """
     # Open in Latin-1 so that we avoid encoding errors.
     # Use codecs.open for Python 2 compatibility
     with codecs.open(os.path.join(here, *file_paths), 'r', 'latin1') as f:
@@ -23,27 +27,31 @@ def find_version(*file_paths):
         return version_match.group(1)
     raise RuntimeError("Unable to find version string.")
 
+
 with codecs.open('README.rst', encoding='utf-8') as f:
     long_description = f.read()
 
+
 setup(
-  name = 'odictliteral',
-  version = find_version('odictliteral.py'),
-  description = 'A tidier way of coding literal OrderedDicts',
-  long_description = long_description,
-  py_modules=['odictliteral'],
+    name='odictliteral',
+    version=find_version('odictliteral.py'),
+    description='A tidier way of coding literal OrderedDicts',
+    long_description=long_description,
+    py_modules=['odictliteral'],
 
-  url = 'https://github.com/ajtowns/odictliteral',
+    url='https://github.com/ajtowns/odictliteral',
 
-  author = 'Anthony Towns',
-  author_email = 'aj@erisian.com.au',
+    author='Anthony Towns',
+    author_email='aj@erisian.com.au',
 
-  license = 'MIT',
+    license='MIT',
 
-  classifiers = [
-    'Development Status :: 4 - Beta',
-    'Intended Audience :: Developers',
-    'Programming Language :: Python :: 2',
-    'Programming Language :: Python :: 3',
-  ],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
+    ],
+
+    tests_require=["pytest"],
 )
